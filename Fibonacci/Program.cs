0695d13110fcdb8465259ecdf0942d3147c8a166
brand <brand@CRISTOPHE_M_L>
2024-06-06 16:20:01 -0400

    Agregar archivos de proyecto.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace suma_del_numero_anterior
+{
+    class progran
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Fibonacci\n");
+
+            Console.WriteLine("Ingrese el numero para iniciar el patron Fibonacci:");
+            int Numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Patron Fibonacci:");
+            for (int i = 0; i < Numero; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+        }
+        static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+
+}
